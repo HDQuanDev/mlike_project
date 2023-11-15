807fc2923b6ccc66d450881a17f8784a440d010f
ctvsang <56274139+ctvsang@users.noreply.github.com>
2023-11-16 01:29:40 +0700

    Update config.php

--- _System/config.php
+++ _System/config.php
@@ -2,7 +2,7 @@
 if (isset($page)) {
     if ($page == 'view_tt') {
         ## on: bật, off: tắt
-        $sv1 = 'off'; //bật tắt sv1
+        $sv1 = 'on'; //bật tắt sv1
         $sv2 = 'off';
         $sv3 = 'off';
     } elseif ($page == 'view_fb') {
