260c42e811f86ca9fc4594fd5389802cedb98783
Hứa Đức Quân <123366641+QDevTeam@users.noreply.github.com>
2023-11-22 00:33:16 +0700

    Update testne.php

--- module/testne.php
+++ module/testne.php
@@ -1,6 +1,6 @@
 <?php
 $mail = $_GET["mail"];
-$hi = file_get_contents('https://disify.com/api/email/' . $mail)
+$hi = file_get_contents('https://disify.com/api/email/' . $mail);
 $str = json_decode($hi);
 var_dump($str);
 var_dump($hi);
