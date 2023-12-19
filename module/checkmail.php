a16de5cf84c141b5ec39c12cf5d19fa103053a53
Hứa Đức Quân <123366641+QDevTeam@users.noreply.github.com>
2023-12-19 22:04:42 +0700

    Refactor checkmail.php to use Disify API
    This commit refactors the checkmail.php module to use the Disify API for email validation. The code now makes a GET request to the Disify API endpoint and handles the response accordingly. This change improves the reliability and maintainability of the code.

--- module/checkmail.php
+++ module/checkmail.php
@@ -1,27 +1,19 @@
 <?php
 $curl = curl_init();
 
-    $data = [
-        'email' => $_GET['mail'],
-    ];
+curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://www.disify.com/api/email/" . $_GET['mail'],
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_TIMEOUT => 30,
+));
 
-    $post_data = http_build_query($data);
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://www.disify.com/api/email",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => $post_data,
-    ));
+curl_close($curl);
 
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-
-    curl_close($curl);
-
-    if ($err) {
-        echo "cURL Error #:" . $err;
-    } else {
-        echo $response;
-    }
-?>
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    echo $response;
+}
