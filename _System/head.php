02210648f5e0d09fe0c9dc836e4800bfe0863c92
Hứa Đức Quân <123366641+QDevTeam@users.noreply.github.com>
2024-01-06 12:53:47 +0700

    Update meta viewport to include maximum-scale

--- _System/head.php
+++ _System/head.php
@@ -72,7 +72,7 @@ if ($row['rule'] == '99') {
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <!--favicon-->
     <link rel="icon" href="<?= $cdn; ?>/images/icon.png" type="image/png" />
     <meta itemprop="image" content="<?= $cdn; ?>/images/top.png">
