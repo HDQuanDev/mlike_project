a24e3c853c0963793c2893e838394c4cd2fcaff4
ctvsang <56274139+ctvsang@users.noreply.github.com>
2024-01-14 20:30:11 +0700

    Update like.php

--- api/buy/facebook/like.php
+++ api/buy/facebook/like.php
@@ -546,7 +546,7 @@ switch ($_GET["act"]) {
                         $array["msg"] = 'Bạn không đủ tiền!';
                     } elseif ($sv4 == 'off') {
                         $array["status"] = 'error';
-                        $array["msg"] = 'Server quá tải mở lại sau 19h ';
+                        $array["msg"] = 'Server quá tải mở lại sau 23h ';
                     } else {
 
                         $fbapi = json_decode(file_get_contents("https://graph.facebook.com/$id?fields=likes.summary(true)&access_token=" . $tokenfb));
