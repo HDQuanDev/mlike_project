2b4e6bc59bb81f107835b4a56c086e74498ca305
Hứa Đức Quân <123366641+QDevTeam@users.noreply.github.com>
2024-01-28 20:58:39 +0700

    Update time check in check_payment.php

--- api/check_payment.php
+++ api/check_payment.php
@@ -9,27 +9,27 @@ $array = [];
 if (isset($login)) {
     if ($_GET['act'] == 'check') {
         $time = time();
-        $tcheck = $time - 300;
+        $tcheck = $time - 500;
         $qc = mysqli_query($db, "SELECT * FROM `momo` WHERE `user` = '$login' AND `time` > '$tcheck' AND `hien` = '0' ORDER BY id DESC LIMIT 1");
         $qc = mysqli_num_rows($qc);
         if ($qc == 1) {
             $result = mysqli_query($db, "SELECT * FROM `momo` WHERE `user` = '$login' AND `time` > '$tcheck' AND `hien` = '0' ORDER BY id DESC LIMIT 1");
             $quan = mysqli_fetch_assoc($result);
-            if ($quan) {
-                $vn = $quan['vnd'];
-                $array['status'] = 'success';
-                $array['username'] = $login;
-                $array['message'] = 'Nạp thành công ' . number_format(floatval($vn)) . 'VNĐ từ ' . $quan['app'] . ' vào tài khoản!';
-                $array['show'] = '1';
-                mysqli_query($db, "UPDATE `momo` SET `hien`= '1' WHERE `id`='$id'");
-                echo json_encode($array);
-                exit();
-            }
+            $vn = $quan['vnd'];
+            $array['status'] = 'success';
+            $array['username'] = $login;
+            $array['message'] = 'Nạp thành công ' . number_format(floatval($vn)) . 'VNĐ từ ' . $quan['app'] . ' vào tài khoản!';
+            $array['show'] = '1';
+            $array['step'] = '1';
+            mysqli_query($db, "UPDATE `momo` SET `hien`= '1' WHERE `id`='$id'");
+            echo json_encode($array);
+            exit();
         } else {
             $array['status'] = 'error';
             $array['username'] = $login;
             $array['message'] = 'Không có giao dịch nào mới!';
             $array['show'] = '0';
+            $array['step'] = '2';
             echo json_encode($array);
             exit();
         }
@@ -38,6 +38,7 @@ if (isset($login)) {
         $array['username'] = $login;
         $array['message'] = 'Không có giao dịch nào mới!';
         $array['show'] = '0';
+        $array['step'] = '3';
         echo json_encode($array);
         exit();
     }
@@ -46,6 +47,7 @@ if (isset($login)) {
     $array['username'] = NULL;
     $array['message'] = 'Bạn chưa đăng nhập!';
     $array['show'] = '2';
+    $array['step'] = '4';
     echo json_encode($array);
     exit();
 }
