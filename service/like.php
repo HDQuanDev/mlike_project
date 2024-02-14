6aff09241eff6a443723e0807418d7e275feda8c
ctvsang <56274139+ctvsang@users.noreply.github.com>
2024-02-14 21:26:46 +0700

    Update like.php

--- service/like.php
+++ service/like.php
@@ -119,7 +119,7 @@ swal({
                         </div>
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" id="flexRadioDefault1" type="radio" name="sv" value="5" data-bs-toggle="collapse" data-bs-target="#sv2" aria-expanded="false" aria-controls="sv2" /><label class="form-check-label" for="flexRadioDefault1">Server Like 5 ( Tốc độ ưu tiên nhanh, <span style="color: red;">Album dể lỗi</span> <span style="color: red;"> max 10k </span> ) <span style="color:red;"><?= $gia5; ?>₫</span> <span class="badge bg-success">Nhanh</span></label>
+                        <input class="form-check-input" id="flexRadioDefault1" type="radio" name="sv" value="5" data-bs-toggle="collapse" data-bs-target="#sv2" aria-expanded="false" aria-controls="sv2" /><label class="form-check-label" for="flexRadioDefault1">Server Like 5 ( Tốc độ ưu tiên nhanh, <span style="color: red;">Album dể lỗi</span> <span style="color: red;"> max 50k </span> ) <span style="color:red;"><?= $gia5; ?>₫</span> <span class="badge bg-success">Nhanh</span></label>
                     </div>
                     <!--
                    <div class="form-check">
@@ -135,7 +135,7 @@ swal({
             </div>
            -->
                     <div class="form-check">
-                        <input class="form-check-input" id="flexRadioDefault1" type="radio" name="sv" value="8" data-bs-toggle="collapse" data-bs-target="#sv8" aria-expanded="false" aria-controls="sv8" /><label class="form-check-label" for="flexRadioDefault1">Server Like 8 ( Tốc độ nhanh, ổn định) <span style="color:Blue;"><?= $gia8; ?>₫</span> <span class="badge bg-success">Tạm ổn</span></label>
+                        <input class="form-check-input" id="flexRadioDefault1" type="radio" name="sv" value="8" data-bs-toggle="collapse" data-bs-target="#sv8" aria-expanded="false" aria-controls="sv8" /><label class="form-check-label" for="flexRadioDefault1">Server Like 8 ( Tốc độ nhanh, ổn định, max 10k) <span style="color:Blue;"><?= $gia8; ?>₫</span> <span class="badge bg-success">Tạm ổn</span></label>
                     </div>
                     <div class="form-check">
                         <input class="form-check-input"  id="flexRadioDefault1" type="radio" name="sv" value="9" data-bs-toggle="collapse" data-bs-target="#sv9" aria-expanded="false" aria-controls="sv9" /><label class="form-check-label" for="flexRadioDefault1">Server Like 9 ( Tốc độ 3k / ngày. chạy được album, max 3k) <span style="color:Blue;"><?= $gia9; ?>₫</span> <span class="badge bg-warning">Nhanh</span></label>
