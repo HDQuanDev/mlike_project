798d6cefe3e9331471cd3541e35fc22a3173fa8c
ctvsang <56274139+ctvsang@users.noreply.github.com>
2024-02-21 21:56:34 +0700

    Update comment.php

--- service/comment.php
+++ service/comment.php
@@ -45,7 +45,7 @@ switch ($_GET['act']) {
                     <input type="hidden" value="<?= $row['token']; ?>" id="token">
                     <label>Chọn Server Comment:</label>
                     <div class="form-check">
-                        <input class="form-check-input" checked id="flexRadioDefault1" type="radio" name="sv" value="1" data-bs-toggle="collapse" data-bs-target="#sv2" aria-expanded="false" aria-controls="sv2" /><label class="form-check-label" for="flexRadioDefault1">Server CMT 1 <span style="color:red;"><?= $gia1; ?>₫</span> <span class="badge bg-success">Đang mở</span></label>
+                        <input class="form-check-input" checked id="flexRadioDefault1" type="radio" name="sv" value="1" data-bs-toggle="collapse" data-bs-target="#sv2" aria-expanded="false" aria-controls="sv2" /><label class="form-check-label" for="flexRadioDefault1">Server CMT 1 ( Clone việt có avt , tốc độ nhanh ) <span style="color:red;"><?= $gia1; ?>₫</span> <span class="badge bg-success">Đang mở</span></label>
                     </div>
 
                     <div class="form-group">
@@ -72,7 +72,6 @@ xin vui lòng không sử dụng kí tự đặc biệt hoặc icon để tránh
                             - Cách lấy link kích vào thời gian đăng bài, kích vào hình ảnh là sai nhé<br>
                             - Vui lòng vào cài đặt => bài viết công khai => ai có thể bình luận vào bài viết của tôi bật mọi người ra trước khi cài <br>
                             - Không hỗ trợ nội dung có chứa icon , kí tự đặc biệt, bài viết trong group <br>
-                            - Hệ thống dùng acc clone có avt, đang update chia sẽ bài viết trên wall cho giống thật <br>
                             - Buff số lượng lớn hơn 3k có thể bị lặp lại acc <p>
 
                     </div>
