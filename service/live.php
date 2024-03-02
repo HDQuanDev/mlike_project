a2b4247a34230e7ade21804c879f6511ca4b4ae7
ctvsang <56274139+ctvsang@users.noreply.github.com>
2024-03-02 19:39:58 +0700

    Update live.php

--- service/live.php
+++ service/live.php
@@ -127,12 +127,12 @@ switch ($_GET['act']) {
                         </div>
                     </div> -->
                     <div class="form-check">
-                        <input class="form-check-input" id="flexRadioDefault1" type="radio" name="sv" value="7" data-bs-toggle="collapse" data-bs-target="#sv2" aria-expanded="false" aria-controls="sv2" /><label class="form-check-label" for="flexRadioDefault1">Server Live 7 (<b><?= $gia7; ?>₫</b>) (Chú ý chỉ nhập ID LÀ SỐ , mắt dao động 70-120%) ( LỖI KHÔNG HỖ TRỢ ) </label>
+                        <input class="form-check-input" disabled id="flexRadioDefault1" type="radio" name="sv" value="7" data-bs-toggle="collapse" data-bs-target="#sv2" aria-expanded="false" aria-controls="sv2" /><label class="form-check-label" for="flexRadioDefault1">Server Live 7 (<b><?= $gia7; ?>₫</b>) (Chú ý chỉ nhập ID LÀ SỐ , mắt dao động 70-120%) ( LỖI KHÔNG HỖ TRỢ ) </label>
                         <div class="collapse" id="sv6">
                         </div>
                     </div> 
                     <div class="form-check">
-                        <input class="form-check-input" checked id="flexRadioDefault1" type="radio" name="sv" value="8" data-bs-toggle="collapse" data-bs-target="#sv2" aria-expanded="false" aria-controls="sv2" /><label class="form-check-label" for="flexRadioDefault1">Server Live 8 (<b><?= $gia8; ?>₫</b>) (Chú ý chỉ nhập ID LÀ SỐ , mắt dao động 70-120%) ( LỖI KHÔNG HỖ TRỢ )</label>
+                        <input class="form-check-input" disabled id="flexRadioDefault1" type="radio" name="sv" value="8" data-bs-toggle="collapse" data-bs-target="#sv2" aria-expanded="false" aria-controls="sv2" /><label class="form-check-label" for="flexRadioDefault1">Server Live 8 (<b><?= $gia8; ?>₫</b>) (Chú ý chỉ nhập ID LÀ SỐ , mắt dao động 70-120%) ( LỖI KHÔNG HỖ TRỢ )</label>
                         <div class="collapse" id="sv6">
                         </div>
                     </div>
