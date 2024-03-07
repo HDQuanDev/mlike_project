84d2c4ead784661af052ad051369b8a285a6f6dc
Hứa Đức Quân <123366641+QDevTeam@users.noreply.github.com>
2024-03-07 21:14:48 +0700

    Fix formatting issue in view_story.php

--- service/view_story.php
+++ service/view_story.php
@@ -56,7 +56,9 @@ switch ($_GET['act']) {
                     <div class="form-check">
                         <input class="form-check-input" checked id="flexRadioDefault1" type="radio" name="sv" value="1" data-bs-toggle="collapse" data-bs-target="#sv1" aria-expanded="false" aria-controls="sv1" /><label class="form-check-label" for="flexRadioDefault1">Server View Story 1 ( Max 2500 ) <span style="color:red;"><?= $gia1; ?>₫</span> <span class="badge bg-success">Đang mở</span></label>
                     </div>
-                    <input class="form-check-input" checked id="flexRadioDefault1" type="radio" name="sv" value="2" data-bs-toggle="collapse" data-bs-target="#sv2" aria-expanded="false" aria-controls="sv2" /><label class="form-check-label" for="flexRadioDefault1">Server View Story 2 ( Đang chờ ) <span style="color:red;"><?= $gia2; ?>₫</span> <span class="badge bg-warning">Sắp mở</span></label>
+                    <div class="form-check">
+                        <input class="form-check-input" checked id="flexRadioDefault1" type="radio" name="sv" value="2" data-bs-toggle="collapse" data-bs-target="#sv2" aria-expanded="false" aria-controls="sv2" /><label class="form-check-label" for="flexRadioDefault1">Server View Story 2 ( Đang chờ ) <span style="color:red;"><?= $gia2; ?>₫</span> <span class="badge bg-warning">Sắp mở</span></label>
+                    </div>
                     <div class="mb-3">
                         <label>Nhập Link Story:</label>
                         <div class="input-group mb-3">
