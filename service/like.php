0ac37cf1f273e912c25b53652793b80ad98a6bba
ctvsang <56274139+ctvsang@users.noreply.github.com>
2024-04-17 15:17:42 +0700

    Update like.php

--- service/like.php
+++ service/like.php
@@ -102,10 +102,10 @@ switch ($_GET['act']) {
                                                             echo 'checked';
                                                         }; ?> id="flexRadioDefault1" disabled type="radio"  name="sv" value="1" data-bs-toggle="collapse" data-bs-target="#sv1" aria-expanded="false" aria-controls="sv1" /><label class="form-check-label" for="flexRadioDefault1">Server Like 1 (Tốc độ hiện tại 15-20 like / 1h, không thể hủy hoàn, chỉ có thể đợi ) </span> <span style="color:red;"><?= $gia1; ?>₫</span> <span class="badge bg-warning">Đang cập nhật</span></label>
                     </div> 
-                   <!--  <div class="form-check">
-                        <input class="form-check-input" id="flexRadioDefault1" type="radio" name="sv" value="2" data-bs-toggle="collapse" data-bs-target="#sv2" aria-expanded="false" aria-controls="sv2" /><label class="form-check-label" for="flexRadioDefault1">Server Like 2 ( Tốc độ nhanh ổn định <span style="color: red;">Album dể lỗi test ít lên được thì cài tiếp</span> Max 100k ) <span style="color:red;"><?= $gia2; ?>₫</span> <span class="badge bg-success">Chậm</span></label>
+                    <div class="form-check">
+                        <input class="form-check-input" id="flexRadioDefault1" type="radio" name="sv" value="2" data-bs-toggle="collapse" data-bs-target="#sv2" aria-expanded="false" aria-controls="sv2" /><label class="form-check-label" for="flexRadioDefault1">Server Like 2 ( Tốc độ trung bình <span style="color: red;">chạy được album</span> Max 50k ) <span style="color:red;"><?= $gia2; ?>₫</span> <span class="badge bg-success">Chậm</span></label>
                     </div>
-                       <div class="form-check">
+                  <!--      <div class="form-check">
                         <input class="form-check-input"  id="flexRadioDefault1" type="radio" name="sv" value="3" data-bs-toggle="collapse" data-bs-target="#sv3" aria-expanded="false" aria-controls="sv3" /><label class="form-check-label" for="flexRadioDefault1">Server Like 3 (Tốc độ nhanh , nên cài bài viết k có like gốc, Bảo Hành tự động 1 tháng, <span style="color: red;">,max 10k </span> ) <span style="color:red;"><?= $gia3; ?>₫</span> <span class="badge bg-success">Hoạt động</span></label> 
 
                     </div> -->
